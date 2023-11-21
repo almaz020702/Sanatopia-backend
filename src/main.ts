2ab31f27aee457a8cc08f311aa1848fd65a53ddb
Almaz <amangeldin.almaz@gmail.com>
2023-11-21 23:55:08 +0600

    feat: Configured Cors

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,10 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: 'http://localhost:5173', // Replace with the origin(s) of your frontend
+    credentials: true, // Enable credentials (cookies, authorization headers)
+  });
   app.setGlobalPrefix('/api');
   await app.listen(process.env.PORT, () => {
     console.log(`Server started on PORT = ${process.env.PORT}`);
