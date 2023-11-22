55ddf7ecbb0bc1f8c1795b0b083210a4d1bdc54e
Almaz <amangeldin.almaz@gmail.com>
2023-11-22 16:21:58 +0600

    docs: Added Documentation

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('Sanatopia')
+    .setDescription('Service for booking Sanatoriums and Hotels')
+    .setVersion('1.0.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('/api/docs', app, document);
   app.enableCors({
     origin: 'http://localhost:5173', // Replace with the origin(s) of your frontend
     credentials: true, // Enable credentials (cookies, authorization headers)
--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -2,14 +2,18 @@
 // eslint-disable-next-line object-curly-newline
 import { Body, Controller, Post, Res } from '@nestjs/common';
 import { Response } from 'express';
+import { ApiBody, ApiOperation, ApiTags } from '@nestjs/swagger';
 import { AuthService } from './auth.service';
 import { CreateUserDto } from './dto/create-user.dto';
-import { AuthResponse } from './interfaces/auth-response.interface';
+import type { AuthResponse } from './interfaces/auth-response.interface';
 
+@ApiTags('Authentication')
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
+  @ApiOperation({ summary: 'User registration' })
+  @ApiBody({ type: CreateUserDto })
   @Post('/registration')
   async registration(
     @Res({ passthrough: true }) res: Response,
@@ -18,6 +22,8 @@ export class AuthController {
     return this.authService.registration(userData, res);
   }
 
+  @ApiOperation({ summary: 'User login' })
+  @ApiBody({ type: CreateUserDto })
   @Post('/login')
   async login(
     @Res({ passthrough: true }) res: Response,
@@ -26,6 +32,7 @@ export class AuthController {
     return this.authService.login(userData, res);
   }
 
+  @ApiOperation({ summary: 'User logout' })
   @Post('/logout')
   async logout(
     @Res({ passthrough: true }) res: Response,
