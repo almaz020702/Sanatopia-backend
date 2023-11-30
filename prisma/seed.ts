a971c37b3f883afc19b9a20956272c141829e3fd
Ansar <annssarr@gmail.com>
2023-12-01 00:59:36 +0600

    add: added prisma seed

---
+++ prisma/seed.ts
@@ -0,0 +1,52 @@
+/* eslint-disable function-paren-newline */
+/* eslint-disable implicit-arrow-linebreak */
+/* eslint-disable @typescript-eslint/no-var-requires */
+import { PrismaClient } from '@prisma/client';
+
+const facilities = require('./fixtures/facilities.json');
+const services = require('./fixtures/services.json');
+const treatments = require('./fixtures/treatments.json');
+
+const prisma = new PrismaClient();
+
+async function main() {
+  const facilitiesPromise = facilities.map((facility) =>
+    prisma.facility.upsert({
+      where: { id: facility.id },
+      update: {},
+      create: facility,
+    }),
+  );
+
+  const servicesPromise = services.map((service) =>
+    prisma.service.upsert({
+      where: { id: service.id },
+      update: {},
+      create: service,
+    }),
+  );
+
+  const treatmentsPromise = treatments.map((treatment) =>
+    prisma.treatment.upsert({
+      where: { id: treatment.id },
+      update: {},
+      create: treatment,
+    }),
+  );
+
+  await prisma.$transaction([
+    ...facilitiesPromise,
+    ...servicesPromise,
+    ...treatmentsPromise,
+  ]);
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
