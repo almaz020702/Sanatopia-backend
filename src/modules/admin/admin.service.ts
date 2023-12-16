b28707b7dcfec3471f9a43ce9b38fd717d81d954
Ansar <annssarr@gmail.com>
2023-12-17 01:00:00 +0600

    feat: admin module, added create role

---
+++ src/modules/admin/admin.service.ts
@@ -0,0 +1,16 @@
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/common/prisma/prisma.service';
+import { CreateRoleDto } from './dto/create-role.dto';
+
+@Injectable()
+export class AdminService {
+  constructor(private readonly prismaService: PrismaService) {}
+
+  async createRole({ name }: CreateRoleDto) {
+    const role = await this.prismaService.role.findUnique({ where: { name } });
+    if (role) {
+      throw new BadRequestException(`Role: ${role.name} already exists`);
+    }
+    return this.prismaService.role.create({ data: { name } });
+  }
+}
