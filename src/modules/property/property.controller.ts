c39cb3dea019e915dc9a0acb09e191ca183a0bd0
Ansar <annssarr@gmail.com>
2023-12-18 02:07:29 +0600

    feat: property module

--- src/modules/property/property.controller.ts
+++ src/modules/property/property.controller.ts
@@ -1,4 +1,24 @@
-import { Controller } from '@nestjs/common';
+/* eslint-disable object-curly-newline */
+/* eslint-disable @typescript-eslint/indent */
+import { Body, Controller, Post, UseGuards } from '@nestjs/common';
+import { UserId } from 'src/common/decorators/user-id.decorator';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
+import { CreatePropertyDto } from './dto/create-property.dto';
+import { PropertyService } from './property.service';
+import { AuthGuard } from '../auth/guards/auth.guard';
 
+@ApiTags('Property')
 @Controller('property')
-export class PropertyController {}
+export class PropertyController {
+  constructor(private readonly propertyService: PropertyService) {}
+
+  @ApiOperation({ summary: 'Creating property' })
+  @Post('')
+  @UseGuards(AuthGuard)
+  async create(
+    @Body() propertyData: CreatePropertyDto,
+    @UserId() userId: number,
+  ) {
+    return this.propertyService.create(propertyData, userId);
+  }
+}
--- src/modules/property/property.module.ts
+++ src/modules/property/property.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
+import { PrismaModule } from 'src/common/prisma/prisma.module';
+import { JwtModule } from '@nestjs/jwt';
 import { PropertyController } from './property.controller';
 import { PropertyService } from './property.service';
 
 @Module({
   controllers: [PropertyController],
   providers: [PropertyService],
+  imports: [
+    PrismaModule,
+    JwtModule.register({
+      secret: process.env.JWT_ACCESS_SECRET,
+      signOptions: {
+        expiresIn: `${process.env.JWT_ACCESS_TTL_IN_MINUTES} minutes`,
+      },
+    }),
+  ],
 })
 export class PropertyModule {}
