b5d8a7742eda44bc7f42c2dc92d8b636c23b48e8
Ansar <annssarr@gmail.com>
2023-12-18 18:15:35 +0600

    change: little changes

--- src/modules/room/interfaces/create-room-response.interface.ts
+++ src/modules/room/interfaces/create-room-response.interface.ts
@@ -1,4 +1,11 @@
-export interface CreateRoomResponse {
-  roomId: number;
+interface IMessage {
   message: string;
+}
+
+export interface CreateRoomResponse extends IMessage {
+  roomId: number;
+}
+
+export interface CreateRoomsResponse extends IMessage {
+  roomIds: number[];
 }
